d84282bda79b8b79a6214f5efe6ae9df4932e939
ThiagoAfonsoBarcelos <t.afonso.barcelos@outlook.com>
2026-04-18 13:01:01 -0300

    Removendo o EXE, ajustes no build do projeto, alinheir onde ficará as DCUs e onde será o EXE, até pra facilitar na hora de procurar no projeto

--- Proj_RaizesDoNordesteAPI.dpr
+++ Proj_RaizesDoNordesteAPI.dpr
@@ -14,19 +14,19 @@ uses
 begin
   try
     //Aqui faz a carga das DLLs para o postgre, para fazer a conexão com o BD
-    //como o projeto é 64bits, ele se encontra em \Win64\Debug, volta duas e vai pra libs
-    SetDllDirectory(PChar(ExtractFilePath(ParamStr(0)) + '..\..\libs'));
+    //como o projeto é 64bits, ele se encontra em \Win64\Debug, volta quatro e vai pra libs
+    SetDllDirectory(PChar(ExtractFilePath(ParamStr(0)) + '..\..\..\..\libs'));
 
     //Faz a conexao com o banco de dados
     TConectarBD.Connect;
 
     untRoutes.RegistrarRotas;
 
-    //Deixei essa porta como padrão, ja do proprio HORSE
+    //Deixei essa porta como padrão, ja do pro  prio HORSE
     THorse.Listen(9000,
       procedure
       begin
-        Writeln('Servidor rodando em ' + THorse.Host + IntToStr(THorse.Port) );
+        Writeln('Servidor rodando em ' + THorse.Host + ':' +IntToStr(THorse.Port) );
       end
     );
   except
